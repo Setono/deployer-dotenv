c4f106ab70e1b7e933edc2087aee1520e1cfbf71
Joachim Løvgaard <joachim@loevgaard.dk>
2020-10-23 13:12:59 +0200

    Update before cache clear task if it exists

--- src/recipe/dotenv.php
+++ src/recipe/dotenv.php
@@ -4,11 +4,25 @@ declare(strict_types=1);
 
 namespace Setono\Deployer\DotEnv;
 
+use Deployer\Deployer;
 use function Deployer\after;
 use function Deployer\before;
 
 require_once 'task/dotenv.php';
 
 after('deploy:update_code', 'dotenv:prepare');
-before('deploy:symlink', 'dotenv:update');
+
+$deployer = Deployer::get();
+
+/**
+ * The task deploy:cache:clear is defined in Symfony related recipes and both the cache clear
+ * and cache warmup tasks sometimes depend in environment variables. Therefore it's a good idea
+ * to have those defined before running these tasks.
+ */
+if($deployer->tasks->has('deploy:cache:clear')) {
+    before('deploy:cache:clear', 'dotenv:update');
+} else {
+    before('deploy:symlink', 'dotenv:update');
+}
+
 before('dotenv:update', 'dotenv:generate');
