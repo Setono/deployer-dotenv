8d20960d3edca9f9e53d51002059b42f9cd60591
Joachim Løvgaard <joachim@loevgaard.dk>
2024-10-03 13:46:06 +0200

    Remove most of old test (we still need to migrate some assertions)

--- tests/recipe/DeployTest.php
+++ tests/recipe/DeployTest.php
@@ -4,61 +4,11 @@ declare(strict_types=1);
 
 namespace Setono\Deployer\DotEnv\recipe;
 
-use Deployer\Console\Application;
-use Deployer\Deployer;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Tester\ApplicationTester;
 
 final class DeployTest extends TestCase
 {
-    private ApplicationTester $tester;
-
-    private Deployer $deployer;
-
-    public static function setUpBeforeClass(): void
-    {
-        set_include_path(get_include_path() . \PATH_SEPARATOR . __DIR__ . '/../../vendor/deployer/deployer');
-
-        // init repository
-        $repository = __DIR__ . '/repository';
-
-        exec("cd $repository && git init");
-        exec("cd $repository && git add .");
-        exec("cd $repository && git config user.name 'Joachim Løvgaard'");
-        exec("cd $repository && git config user.email 'joachim@setono.com'");
-        exec("cd $repository && git commit -m 'first commit'");
-    }
-
-    protected function setUp(): void
-    {
-        // init deployer and application tester0
-        $console = new Application();
-        $console->setAutoExit(false);
-        $this->tester = new ApplicationTester($console);
-
-        $this->deployer = new Deployer($console);
-        Deployer::loadRecipe(__DIR__ . '/deploy.php');
-        $this->deployer->init();
-    }
-
-    protected function tearDown(): void
-    {
-        foreach ($this->deployer->hosts as $host) {
-            $deployPath = $host->get('deploy_path');
-            exec("rm -rf $deployPath");
-        }
-    }
-
-    public static function tearDownAfterClass(): void
-    {
-        $git = __DIR__ . '/repository/.git';
-        exec("rm -rf $git");
-    }
-
-    /**
-     * @test
-     */
     public function it_deploys(): void
     {
         $this->tester->run([
--- tests/recipe/repository/README.md
+++
@@ -1,1 +0,0 @@
-# This is a fake repository used for testing
