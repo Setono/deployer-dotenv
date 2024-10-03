6f65fd8f783be8f9613d15e0b6520907ba2553fc
Joachim Løvgaard <joachim@loevgaard.dk>
2024-10-03 13:41:16 +0200

    Fix dep issues

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -7,4 +7,19 @@ return (new Configuration())
     ->addPathToExclude(__DIR__ . '/tests')
     ->addPathToScan(__DIR__ . '/src', false)
     ->ignoreErrorsOnPackage('deployer/deployer', [ErrorType::UNUSED_DEPENDENCY])
+    ->ignoreUnknownFunctions([
+        'Deployer\after',
+        'Deployer\ask',
+        'Deployer\askConfirmation',
+        'Deployer\before',
+        'Deployer\get',
+        'Deployer\has',
+        'Deployer\input',
+        'Deployer\invoke',
+        'Deployer\output',
+        'Deployer\run',
+        'Deployer\task',
+        'Deployer\test',
+        'Deployer\upload',
+    ])
 ;
