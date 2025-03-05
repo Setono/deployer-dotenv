e1c0d51de6ef1049be20262d73943f050682c538
Joachim Løvgaard <joachim@loevgaard.dk>
2025-03-05 13:45:39 +0100

    Exclude vendor dir

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -4,6 +4,7 @@ use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
 use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
 
 return (new Configuration())
-    ->addPathToExclude(__DIR__ . '/tests')
     ->addPathToScan(__DIR__ . '/src', false)
+    ->addPathToExclude(__DIR__ . '/tests')
+    ->addPathToExclude(__DIR__ . '/vendor')
 ;
